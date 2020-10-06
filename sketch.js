93612861be8014926cf7f44e6142a06eb98e148a
VIDHI3030 <72209528+VIDHI3030@users.noreply.github.com>
2020-10-06 18:40:00 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,27 +1,28 @@
 var box1,box2;
 function setup() {
   createCanvas(800,400);
- box1= createSprite(400, 200, 50, 50);
- box2= createSprite(200,200,50,50);
+ box1= createSprite(400, 400, 50, 50);
+ box2= createSprite(50,50,50,50);
  box1.shapeColor="blue";
  box2.shapeColor="hotpink";
+ box1.velocityX=-1;
+ box1.velocityY=-1;
+ box2.velocityX=1;
+ box2.velocityY=1;
 }
 
 function draw() {
   background(255,255,255); 
-  box1.x= mouseX;
-  box1.y= mouseY;
-  if (box1.x-box2.x<box1.width/2+box2.width/2 && box2.x-box1.x<box1.width/2+box2.width/2
-    && box1.y-box2.y<box1.height/2+box2.height/2 && box2.y-box1.y<box1.height/2+box2.height/2   ){
+  if (detectcollision(box1,box2)){
       box1.shapeColor="green";
       box2.shapeColor="black";
       
-    }else{
+    /*}else{
       box1.shapeColor="blue";
       box2.shapeColor="hotpink";
-
+*/
     }
-
+bounceOff(box1,box2);
   drawSprites();
 
 }
